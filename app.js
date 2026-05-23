f39c762413c02f6de58b38716741c22e164b497b
Lupo-Albi <luandersonalbuquerque@gmail.com>
2026-05-22 22:54:05 -0300

    🎨 Change name and position of variables for clarity and consistency

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-const initialInvestment = 50;
-const timeSpanInDays = 7;
-const timeSpanInMs = timeSpanInDays * 24 * 60 * 60 * 1000
+// Date Settings
 const initialDate = new Date('2026-01-01T00:00:00-03:00');
 const initialDateInMs = initialDate.getTime();
 const finalDate = new Date('2026-05-22T00:00:00-03:00');
 const finalDateInMs = finalDate.getTime();
 
+if (finalDateInMs < initialDateInMs) {
+  console.log("finalDate cannot be prior to initialDate");
+  return;
+}
+
+// Value settings
+const initialInvestment = 50;
+const increment = 1;
+const timeSpanInDays = 7;
+const timeSpanInMs = timeSpanInDays * 24 * 60 * 60 * 1000;
+
 if (initialInvestment <= 0 || timeSpanInDays <= 0) {
     console.log(`It's not possible to save ${initialInvestment} in the time span of ${timeSpanInDays} days. Try positive values.`);
 }
 
-if (finalDateInMs < initialDateInMs) {
-    console.log('finalDate cannot be prior to initialDate');
-    return;
-}
-
-const increment = 1;
 let savings = 0;
-let timeTravel = initialDateInMs;
+let dateToCheck = initialDateInMs;
 let currentInvestment = initialInvestment;
 
 do {
   savings += currentInvestment;
-  timeTravel += timeSpanInMs;
+  dateToCheck += timeSpanInMs;
   currentInvestment += extra;
-} while (timeTravel < finalDateInMs);
+} while (dateToCheck < finalDateInMs);
 
 console.log(`Total savings: ${savings}`);
